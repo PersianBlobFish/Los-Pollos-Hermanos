21eda786a288d20adb2aa3248c2c5fdbd54da551
PersianBlobFish <nguyentran2436@gmail.com>
2024-11-18 13:50:41 +0700

    Initial commit

---
+++ ModifiedDrivetrain-2024-11-18T06-39-50/src/main.cpp
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\Admin                                            */
+/*    Created:      Fri Nov 01 2024                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// Drivetrain           drivetrain    1, 2, 3, 4      
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+}
