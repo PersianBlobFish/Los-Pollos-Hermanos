05dac321eb99cfbdbfa0311cc32adcfd52cc0943
PersianBlobFish <nguyentran2436@gmail.com>
2025-01-24 09:32:39 +0700

    ladyBrown execution test

--- Lemlib/src/main.cpp
+++ Lemlib/src/main.cpp
@@ -125,6 +125,14 @@ ASSET(test_txt); // '.' replaced with "_" to make c++ happy
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
+    //Set the stopping point for Lady Brown
+    while (potentiometer.get_value() > 700) {
+        ladyBrown.move_velocity(0);
+    }
+    //Set the brake mode for Lady Brown
+    ladyBrown.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+    //Alliance wall stake
     conveyor.move_velocity(200);
     pros::delay(2000);
     conveyor.move_velocity(0);
@@ -135,6 +143,9 @@ void autonomous() {
     while (true) {
         lemlib::Pose pose = chassis.getPose();
         if (pose.x >= -30 && pose.y >= -18 ) {
+            while (potentiometer.get_value() < 2060) {
+                ladyBrown.move_velocity(100);
+            }
             intake.move_velocity(200)&&conveyor.move_velocity(200);
             pros::delay(2000);
             intake.move_velocity(0)&&conveyor.move_velocity(0);
@@ -143,39 +154,22 @@ void autonomous() {
         // Delay to save resources
         pros::delay(10);
     }
-
-    // // Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    // chassis.moveToPose(20, 15, 90, 4000);
-    // // Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    // chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // // cancel the movement after it has traveled 10 inches
-    // chassis.waitUntil(10);
-    // chassis.cancelMotion();
-    // // Turn to face the point x:45, y:-45. Timeout set to 1000
-    // // dont turn faster than 60 (out of a maximum of 127)
-    // chassis.turnToPoint(45, -45, 1000, {.maxSpeed = 60});
-    // // Turn to face a direction of 90º. Timeout set to 1000
-    // // will always be faster than 100 (out of a maximum of 127)
-    // // also force it to turn clockwise, the long way around
-    // chassis.turnToHeading(90, 1000, {.direction = AngularDirection::CW_CLOCKWISE, .minSpeed = 100});
-    // // Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // // following the path with the back of the robot (forwards = false)
-    // // see line 116 to see how to define a path
-    // chassis.follow(example_txt, 15, 4000, false);
-    // // wait until the chassis has traveled 10 inches. Otherwise the code directly after
-    // // the movement will run immediately
-    // // Unless its another movement, in which case it will wait
-    // chassis.waitUntil(10);
-    // pros::lcd::print(4, "Traveled 10 inches during pure pursuit!");
-    // // wait until the movement is done
-    // chassis.waitUntilDone();
-    // pros::lcd::print(4, "pure pursuit finished!");
 }
 
 /**
  * Runs in driver control
  */
 void opcontrol() {
+    ladyBrown.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+        //Controller (arcade drive)
+    while (true) {
+        // get joystick positions
+        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+        // move the chassis with curvature drive
+        chassis.arcade(leftY, rightX);
+    }
 
     while (true) {
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
@@ -222,15 +216,6 @@ void opcontrol() {
             ladyBrown.move_velocity(0);
         }
     }
-    // controller
-    // loop to continuously update motors
-    while (true) {
-        // get joystick positions
-        int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-        // move the chassis with curvature drive
-        chassis.arcade(leftY, rightX);
         // delay to save resources
         pros::delay(10);
-    }
 }
