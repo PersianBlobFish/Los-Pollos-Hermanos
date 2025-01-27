3a4d1dc012320af3fe3aa59a09b4b6ba7aff9652
PersianBlobFish <nguyentran2436@gmail.com>
2025-01-27 17:18:24 +0700

    resolve merge conflict

--- Lemlib/src/main.cpp
+++ Lemlib/src/main.cpp
@@ -1,21 +1,215 @@
 #include "main.h"
+#include "pros/apix.h"
+#include "pros/colors.h"
+#include <llemu.h>
+#include <llemu.hpp>
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "lemlib-tarball/api.hpp" // IWYU pragma: keep
-#define POTENTIOMETER_PORT 'A'
 
-// @brief Position value of Potentiometer
+/// @brief Position value of Potentiometer
 const double POTENTIOMETER_POSITION = 1050;
+
+/// @brief Track width in inch.
+const double TRACK_WIDTH = 14.05;
+
+/// @brief Wheel circumference in mm.
+const double WHEEL_CIRC = 262.0;
+
+/// @brief Number of output gear teeth divided by number of input gear teeth.
+const double GEAR_RATIO = 36.0 / 60.0;
+
+/// @brief The speed of the conveyor as a percentage of its max speed (200 rpm).
+const double CONVEYOR_SPEED_PERCENT = 100;
+
+/// @brief The speed of the ladybrown as a percentage of its max speed (200 rpm).
+const double LADYBROWN_SPEED_PERCENT = 75;
+
+/// @brief Vision sensor signature ID for the red donut.
+const uint32_t RED_SIG_ID = 1;
+
+/// @brief Vision sensor signature ID for the blue donut.
+const uint32_t BLUE_SIG_ID = 2;
+
+/// @brief Delay in ms for the conveyor to stop after a donut of the wrong color
+/// is detected.
+const uint32_t CONVEYOR_DELAY = 350;
+
+/// @brief Delay in ms for the conveyor to start after ejecting a donut of the
+/// wrong color.
+const uint16_t CONVEYOR_HALT = 200;
+
+/// @brief Distance in degrees for the conveyor to move after a donut of the
+/// wrong color is detected.
+const double CONVEYOR_STOP_DISTANCE = 770;
+
+/// @brief Minimum screen coverage for the donut to be detected using the vision
+/// sensor.
+const int MIN_SCREEN_COVERAGE = 100;
+
+/// @brief Gear ratio for the conveyor motor.
+const double CONVEYOR_GEAR_RATIO = 12.0 / 18.0;
+
+/// @brief Number of teeth in the conveyor output sprocket.
+const int CONVEYOR_OUT_TEETH = 12;
+
+/// @brief Number of links in the conveyor.
+const int CONVEYOR_LINKS = 74;
+
+/// @brief Offset in links between the first and second conveyor hooks.
+const int CONVEYOR_HOOK_2_OFFSET = 31;
+
+/// @brief Number of motor degrees for the conveyor to make a full revolution.
+const double CONVEYOR_REVOLUTION_DEGREES = double(CONVEYOR_LINKS) /
+                                           double(CONVEYOR_OUT_TEETH) * 360.0 *
+                                           CONVEYOR_GEAR_RATIO;
+
+/// @brief Offset in degrees between the first and second conveyor hook.
+const double CONVEYOR_HOOK_2_OFFSET_DEGREES = double(CONVEYOR_HOOK_2_OFFSET) /
+                                              double(CONVEYOR_OUT_TEETH) *
+                                              360.0 * CONVEYOR_GEAR_RATIO;
+
+/// @brief Distance in conveyor motor degrees between the vision sensor and the
+/// top of the conveyor.
+const double DISTANCE_VISION_TO_TOP = 520;
+
+/// @brief Enum for the colors of the donuts.
+enum class DONUT_COLOR { RED, BLUE };
+
+/// @brief Enum for motor spinning states.
+enum class SPIN_STATE { STOP, FORWARD, REVERSE };
+
+/// @brief Enum for donut color sorting states.
+enum class SORTING_STATE { NOT_DETECTED, STANDBY, STOP };
+
+// Define constants for screen dimensions and colors (do not fucking change this!!!) (in Pixel)
+const int SCREEN_WIDTH = 480;
+const int SCREEN_HEIGHT = 240;
+const int REGION_WIDTH = SCREEN_WIDTH / 2;
+const int REGION_HEIGHT = SCREEN_HEIGHT / 2;
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
-pros::Motor ladyBrown(9, pros::MotorGearset::green); // left front motor
-pros::Motor intake(-6, pros::MotorGearset::green); // intake motor
-pros::Motor conveyor(-7, pros::MotorGearset::green); // conveyor motor
-pros::MotorGroup leftMotors({-1, -2}, pros::MotorGearset::green); // left motor group
-pros::MotorGroup rightMotors({3, 4}, pros::MotorGearset::green); // right motor group
-pros::ADIAnalogIn potentiometer(POTENTIOMETER_PORT);// line sensor on port 1
+pros::Motor ladyBrown(9, pros::MotorGearset::green); // Left front motor
+pros::Motor intake(-6, pros::MotorGearset::green); // Intake motor
+pros::Motor conveyor(-7, pros::MotorGearset::green); // Conveyor motor
+pros::MotorGroup leftMotors({-1, -2}, pros::MotorGearset::green); // Left motor group
+pros::MotorGroup rightMotors({3, 4}, pros::MotorGearset::green); // Right motor group
+pros::ADIAnalogIn potentiometer('a');// Potentiometer on port A
+
+// Inertial Sensor on port 13
+pros::Imu imu(13);
+
+//
+int autonomousSelection = 0; // Default autonomous selection
+
+// Utility function to draw a rectangle with a color
+void drawAutonRegion(int x, int y, int width, int height) {
+    pros::screen::fill_rect(x, y, x + width, y + height);
+}
+
+// Utility function to clear screen
+void clearScreen() {
+    pros::screen::set_pen(pros::c::COLOR_BLACK);
+    pros::screen::fill_rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+// Utility function to draw the autonomous menu
+void drawAutonMenu() {
+    clearScreen();
+    pros::screen::set_pen(pros::c::COLOR_RED);
+    drawAutonRegion(0, 0, REGION_WIDTH, REGION_HEIGHT);          // Top-left
+    drawAutonRegion(REGION_WIDTH, 0, REGION_WIDTH, REGION_HEIGHT);  // Top-right
+    pros::screen::set_pen(pros::c::COLOR_BLUE);
+    drawAutonRegion(0, REGION_HEIGHT, REGION_WIDTH, REGION_HEIGHT); // Bottom-left
+    drawAutonRegion(REGION_WIDTH, REGION_HEIGHT, REGION_WIDTH, REGION_HEIGHT); // Bottom-right
+}
+
+// Utility function to select an autonomous 
+void selectAutonRegion(int x, int y) {
+    clearScreen();
+    if (y <= 120 ) {
+        pros::screen::set_pen(pros::c::COLOR_RED);
+    } else {
+        pros::screen::set_pen(pros::c::COLOR_BLUE);
+    }
+
+    drawAutonRegion(x, y, REGION_WIDTH, REGION_HEIGHT);
+
+    // Wait for another screen press to return to the menu
+    pros::screen::touch_callback(drawAutonMenu, TOUCH_PRESSED);
+}
+
+// Function to proccess the touch screen input
+void autonSelection() {
+    drawAutonMenu();
+    while (true) {
+        pros::screen_touch_status_s_t status = pros::screen::touch_status();
+        auto touch = pros::screen::touch_status();
+        int x = touch.x;
+        int y = touch.y;
+
+        // Determine which region was pressed and highlight it
+        if (x < REGION_WIDTH && y < REGION_HEIGHT) {
+            selectAutonRegion(0, 0); // Top-left
+        } else if (x >= REGION_WIDTH && y < REGION_HEIGHT) {
+            selectAutonRegion(REGION_WIDTH, 0); // Top-right
+        } else if (x < REGION_WIDTH && y >= REGION_HEIGHT) {
+            selectAutonRegion(0, REGION_HEIGHT); // Bottom-left
+        } else {
+            selectAutonRegion(REGION_WIDTH, REGION_HEIGHT); // Bottom-right
+        }
+    }
+}
+
+// Define autonomous routine functions
+void leftAutonRed() {
+    pros::lcd::clear();
+    pros::lcd::set_text(3, "Executing LEFT AUTON - RED");
+    // Add your autonomous code for left red here
+}
+
+void rightAutonRed() {
+    pros::lcd::clear();
+    pros::lcd::set_text(3, "Executing RIGHT AUTON - RED");
+    //Setting post manually
+    chassis.setPose({-57, -14, 325});
+
+    //Follow set path
+    chassis.follow(decoder["path1"],15,2000,false);
+    chassis.follow(decoder["path2"],15,2000);
+    chassis.follow(decoder["path3"],15,2000);
+    chassis.follow(decoder["path4"],15,2000,false);
+
+    //Spin the intake/conveyor motors
+    motorSpin(200, 2000);
+
+    // Execute the rest of the autonomous routine
+    while (true) {
+        lemlib::Pose pose = chassis.getPose();
+        if (pose.x >= -30 && pose.y >= -18 ) {
+            moveToAngle(23.94);
+            intake.move_velocity(200)&&conveyor.move_velocity(200);
+            pros::delay(2000);
+            intake.move_velocity(0)&&conveyor.move_velocity(0);
+        }
+        // Delay to save resources
+        pros::delay(10);
+    }
+}
+
+void leftAutonBlue() {
+    pros::lcd::clear();
+    pros::lcd::set_text(3, "Executing LEFT AUTON - BLUE");
+    // Add your autonomous code for left blue here
+}
+
+void rightAutonBlue() {
+    pros::lcd::clear();
+    pros::lcd::set_text(3, "Executing RIGHT AUTON - BLUE");
+    // Add your autonomous code for right blue here
+}
 
 void moveToAngle(int targetAngle) { 
     int targetValue = targetAngle * 4095 / 265; // Map 0-300 degrees to 0-4095 range
@@ -43,13 +237,12 @@ void motorSpin(int speed, int duration) {
     conveyor.move_velocity(0) && intake.move_velocity(0);
 }
 
-// Inertial Sensor on port 13
-pros::Imu imu(13);
+
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              14.05, // 10 inch track width
+                              TRACK_WIDTH, // 10 inch track width
                               lemlib::Omniwheel::OLD_325, // using old 3.25" omnis
                               340, // drivetrain rpm is 360
                               2 // horizontal drift is 2. If we had traction wheels, it would have been 8
@@ -106,7 +299,15 @@ lemlib::Chassis chassis(drivetrain, linearController, angularController, sensors
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
+    while (imu.is_calibrating()) {
+        pros::delay(10);
+    }
+    //Print message to brain screen
+    pros::lcd::set_text(2, "Inertial sensor calibrated!");
 
+    //Record the lastest preesed event
+    pros::screen::touch_callback(autonSelection, TOUCH_PRESSED);
+    
     // thread to for brain screen and position logging
     pros::Task screenTask([&]() {
         while (true) {
@@ -122,6 +323,11 @@ void initialize() {
     });
 }
 
+void pre_auton() {
+    // initialize the inertial sensor
+    imu.reset();
+}
+
 /**
  * Runs while the robot is disabled
  */
@@ -131,8 +337,6 @@ void disabled() {}
  * runs after initialize if the robot is connected to field control
  */
 void competition_initialize() {}
-
-// get a path used for pure pursuit
 // this needs to be put outside a function
 ASSET(test_txt); // '.' replaced with "_" to make c++ happy
 lemlib_tarball::Decoder decoder(test_txt);
@@ -142,37 +346,30 @@ lemlib_tarball::Decoder decoder(test_txt);
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    //Set the stopping point for Lady Brown
+    //Stop Lady Brown at the starting point
     while (potentiometer.get_value() > 700) {
         ladyBrown.move_velocity(0);
     }
     //Set the brake mode for Lady Brown
     ladyBrown.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 
-    //Setting pose manually
-    chassis.setPose({-57, -14, 325});
-
-    chassis.follow(decoder["path1"],15,2000,false);
-    chassis.follow(decoder["path2"],15,2000);
-    chassis.follow(decoder["path3"],15,2000);
-    chassis.follow(decoder["path4"],15,2000,false);
-
-    //Alliance wall stake
-    motorSpin(200, 2000);
-
-    //chassis.follow(test_txt, 15, 4000);
-    // Follow set path
-    while (true) {
-        lemlib::Pose pose = chassis.getPose();
-        if (pose.x >= -30 && pose.y >= -18 ) {
-            moveToAngle(23.94);
-            intake.move_velocity(200)&&conveyor.move_velocity(200);
-            pros::delay(2000);
-            intake.move_velocity(0)&&conveyor.move_velocity(0);
-        }
-        // Update motors
-        // Delay to save resources
-        pros::delay(10);
+    // Select autonomous routine
+    switch (autonomousSelection) {
+        case 0:
+            leftAutonRed();
+            break;
+        case 1:
+            rightAutonRed();
+            break;
+        case 2:
+            leftAutonBlue();
+            break;
+        case 3:
+            rightAutonBlue();
+            break;
+        default:
+            pros::lcd::set_text(2, "No autonomous selected!");
+            break;
     }
 }
 
@@ -180,16 +377,19 @@ void autonomous() {
  * Runs in driver control
  */
 void opcontrol() {
+
+    //Stop Lady Brown at the starting point
     ladyBrown.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 
-        //Controller (arcade drive)
+        //Controller (tank drive)
     while (true) {
-        // get joystick positions
+        // Get joystick positions
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
         // move the chassis with curvature drive
         chassis.tank(leftY, rightY);
 
+        // lady brown control
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
             ladyBrown.move_velocity(100);
         } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
@@ -198,6 +398,7 @@ void opcontrol() {
             ladyBrown.move_velocity(0);
         }
 
+        // Intake and conveyor control
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
             intake.move_velocity(200);
             conveyor.move_velocity(200);
@@ -209,24 +410,18 @@ void opcontrol() {
             intake.move_velocity(0);
         }
 
+        // Set lady brown to a specific position (intake)
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
-            while (potentiometer.get_value() < 1050) {
+            while (potentiometer.get_value() < POTENTIOMETER_POSITION) {
                 ladyBrown.move_velocity(100);
             }
-            if (potentiometer.get_value() > 1050) {
+            if (potentiometer.get_value() > POTENTIOMETER_POSITION) {
                 ladyBrown.move_velocity(0);
             }
         } else {
            ladyBrown.move_velocity(0);
         }
 
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-            ladyBrown.move_velocity(100);
-        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
-            ladyBrown.move_velocity(-100);
-        } else {
-            ladyBrown.move_velocity(0);
-        }
         // delay to save resources
         pros::delay(10);
     }
